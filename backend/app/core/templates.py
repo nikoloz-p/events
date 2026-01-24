7dafd0ae076fdfe4f5d00ef01b818897c97faf10
Nika <pichkhaianikoloz@gmail.com>
2026-01-24 07:27:42 +0400

    fixed jinja templates path

--- backend/app/core/templates.py
+++ backend/app/core/templates.py
@@ -1,7 +1,7 @@
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parents[1]
 
 templates = Jinja2Templates(
     directory=BASE_DIR / "templates"
